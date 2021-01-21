6209efd0130c23f4281c40a50e2e33eb14f5fe1d
Roland C. Dowdeswell <elric@imrryr.org>
2021-01-21 21:20:38 +0000

    Only import the required keys in init containers

--- key-importer/entrypoint.sh
+++ key-importer/entrypoint.sh
@@ -1,12 +1,32 @@
 #!/bin/sh
+
+set -x
+
 mkdir -p /var/tezos/client
 chmod -R 777 /var/tezos/client
 
-echo $ACCOUNTS | jq -c --raw-output .[] | while read line; do
-    key=$(echo $line | jq -r '.key')
-    name=$(echo $line | jq -r '.name')
-    keytype=$(echo $line | jq -r '.type')
-    protocol=$(echo $CHAIN_PARAMS | jq -r '.protocol_hash')
-    printf "\nImporting key ${name}\n"
-    tezos-client -d /var/tezos/client --protocol ${protocol} import ${keytype} key ${name} unencrypted:${key} -f
-done
+import_key() {
+    echo $ACCOUNTS | \
+    jq -r ".[] | select(.name == \"baker$1\") | .type, .name, .key" | (
+	read keytype
+	read name
+	read key
+
+	protocol=$(echo $CHAIN_PARAMS | jq -r '.protocol_hash')
+
+	tezos-client -d /var/tezos/client --protocol ${protocol} \
+	    import ${keytype} key ${name} unencrypted:${key} -f
+    )
+}
+
+#
+# For some reason, we all need to have baker0's key.  We can likely
+# fix this in the future, I would imagine.
+
+import_key 0
+
+HOSTNAME=$(hostname)
+MY_NODE=${HOSTNAME##tezos-baking-node-}
+if [ "$MY_NODE" != "$HOSTNAME" ]; then
+    import_key $MY_NODE
+fi
