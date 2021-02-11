65c023ef6d7f44a4e3ae924ea0004bd7ed9a58b2
Daniel Feygin <daniel.feygin@gmail.com>
2021-02-10 20:46:46 -0600

    added cloudwatch log collection to EKS cluster

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -36,14 +36,43 @@ helm_values["tezos_k8s_images"] = images;
 
 const vpc = new awsx.ec2.Vpc("tezos-vpc", {});
 
+function createWorkerNodeRole(name: string): aws.iam.Role {
+    const managedPolicyArns: string[] = [
+        "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+        "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
+        "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
+        "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy",
+    ];
+    
+    const role = new aws.iam.Role(name, {
+        assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal({
+            Service: "ec2.amazonaws.com",
+        }),
+    });
+
+    let counter = 0;
+    for (const policy of managedPolicyArns) {
+        // RolePolicyAttachment is attached to the role upon instantiation, so
+        // it doesn't need to be returned or assigned.
+        const rpa = new aws.iam.RolePolicyAttachment(`${name}-policy-${counter++}`,
+            { policyArn: policy, role: role },
+        );
+    }
+
+    return role;
+}
+
+
 // Create an EKS cluster.
-const cluster = new eks.Cluster("tq-private-chain", {
+const resourceName = "tq-private-chain"
+const cluster = new eks.Cluster(resourceName, {
     vpcId: vpc.id,
     subnetIds: vpc.publicSubnetIds,
     instanceType: "t2.xlarge",
     desiredCapacity: 40,
     minSize: 3,
     maxSize: 50,
+    instanceRole: createWorkerNodeRole(resourceName),
 })
 
 const ns = new k8s.core.v1.Namespace("tezos", {metadata: {name:"tezos",}},
@@ -79,6 +108,34 @@ const nginxIngress = new k8s.helm.v2.Chart("nginx-ingress", {
 // Enable proxy protocol v2 on the target groups:
 //   https://github.com/kubernetes/ingress-nginx/issues/5051#issuecomment-685736696
 // Create a A record in the dns domain for which a certificate was created.
+
+
+// Create Cloudwatch namespace in EKS cluster
+const amazonCloudwatchNamespace = new k8s.core.v1.Namespace("amazon-cloudwatch",
+                                    {metadata: {name:"amazon-cloudwatch",}},
+                                    {provider: cluster.provider});
+
+
+// Create Fluent Bit configuration in EKS cluster
+const clusterInfoConfigMap = new k8s.core.v1.ConfigMap("fluent-bit-cluster-info", {
+    metadata: {
+        name: "fluent-bit-cluster-info",
+        namespace: "amazon-cloudwatch",
+    },
+    data: {
+        "cluster.name": cluster.eksCluster.name,
+        "http.server": "Off",
+        "http.port": "",
+        "read.head": "On",
+        "read.tail": "Off",
+        "logs.region": new pulumi.Config("aws").require("region"),
+    },
+}, {provider: cluster.provider});
+
+// Deploy Fluent Bit DaemonSet to the EKS cluster
+const fluentbit = new k8s.yaml.ConfigFile("fluent-bit", {file: "./fluent-bit.yaml"},
+    {provider: cluster.provider}
+);
 
 // Export the cluster's kubeconfig.
 export const kubeconfig = cluster.kubeconfig;
